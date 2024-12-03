5b50bfe88c6735e4dd78c475100f73ba8c66692e
Adrian Graham <adriangraham@gmail.com>
2024-12-03 10:20:08 -0800

    prep for dynamo-bao

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   AWS_REGION: process.env.AWS_REGION || 'us-west-2',
-  TABLE_NAME: process.env.TABLE_NAME || 'raftjs-dynamo-dev'
+  TABLE_NAME: process.env.TABLE_NAME || 'dynamo-bao-dev'
 };
